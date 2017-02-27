b00896e7fec92d0add209595fc2d2e7c3400ab1c
jie.huang <jie.huang@ele.me>
2017-02-27 17:08:30 +0800

    fix gauge merge crash and add gauge timestamp

--- utilities/tsdb/GaugeMerger.cc
+++ utilities/tsdb/GaugeMerger.cc
@@ -23,17 +23,25 @@ namespace rocksdb {
 
         while (old_slot != -1 || new_slot != -1) {
             if (old_slot == new_slot && old_slot != -1) {
+                int64_t old_time = existStream->getNextValue();
+                int64_t old_value = existStream->getNextValue();
+                int64_t new_time = newStream->getNextValue();
+                int64_t new_value = newStream->getNextValue();
+
                 //put count/sum value into merge value
-                writer->append(old_slot, newStream->getNextValue());
+                writer->append(old_slot, new_time >= old_time ? new_time : old_time);
+                writer->appendValue(new_time >= old_time ? new_value : old_value);
                 //reset old/new slot for next loop
                 old_slot = existStream->getNextTimestamp();
                 new_slot = newStream->getNextTimestamp();
             } else if (old_slot != -1 && (new_slot == -1 || old_slot < new_slot)) {
                 writer->append(old_slot, existStream->getNextValue());
+                writer->appendValue(existStream->getNextValue());
                 //reset old slot for next loop
                 old_slot = existStream->getNextTimestamp();
             } else if (new_slot != -1 && (old_slot == -1 || new_slot < old_slot)) {
                 writer->append(new_slot, newStream->getNextValue());
+                writer->appendValue(newStream->getNextValue());
                 //reset new slot for next loop
                 new_slot = newStream->getNextTimestamp();
             }
